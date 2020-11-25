c2811f43660968b1749766054b93a9402b1b0d8b
KimHyeWon <keykeykey193@gmail.com>
2020-11-25 16:17:22 +0900

    :hammer: Change StatelessWidget's code, comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp());    // 앱 실행
 }
 
 class MyApp extends StatelessWidget {
+  // 상태 변경이 없는 위젯. 한번 UI가 그려지면 그대로 있음.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    // UI를 만드는 부분.
+    return new MaterialApp(
+      // 구글 기본 디자인인 Material Design 을 써서 앱을 만든다.
+        title: 'My Practice Flutter App',
+        home: Scaffold
+        appBar: AppBar(title: Text("타이틀")),
+    // 앱의 상단 타이틀
+    body: Text("첫 플러터 앱")
+    // 앱 화면에 표시되는 텍스트
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
